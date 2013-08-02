3af8a617e2cae0b45013a8e9bf4177f5a3f0c633
EBO <emanuele.buchicchio@gmail.com>
2013-08-02 08:19:59 +0200

    mapview demo

--- MapKitDemo/ViewController.m
+++ MapKitDemo/ViewController.m
@@ -7,18 +7,30 @@
 //
 
 #import "ViewController.h"
+#define CMU_LAT 40.4435037
+#define CMU_LONG -79.9415706
+#define UPPITT_LAT 40.444089
+#define UPPITT_LONG -79.953389
+#define MAX_ZOMBIES 100
+#define MAX_WEAPONS 40
 
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
+@synthesize mapView;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    CLLocationCoordinate2D center = CLLocationCoordinate2DMake(CMU_LAT, CMU_LONG);
+    [self.mapView setRegion:MKCoordinateRegionMake(center,MKCoordinateSpanMake(0.1,0.1))];
+    
+    self.mapView.showsUserLocation=YES;
 }
+
 
 - (void)didReceiveMemoryWarning
 {
